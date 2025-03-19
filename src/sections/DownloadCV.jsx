e5ce48c13cda85bec796876cd0b6fc385aeadc67
SABDELS7 <salmiabdderrahmane@gmail.com>
2025-03-19 00:40:20 +0000

    fix the cv btn"

--- src/sections/DownloadCV.jsx
+++ src/sections/DownloadCV.jsx
@@ -4,7 +4,7 @@ const DownloadCVButton = () => {
   return (
     <a
       href="../../public/textures/CV_Abderrahmane_Salmi.pdf"
-      download="Abderrahmane_Salmi_CV.pdf"
+      download="CV_Abderrahmane_Salmi.pdf"
       className="fixed z-[10000] bottom-20 right-4 bg-gray-800 text-white p-3 rounded-full shadow-lg hover:bg-gray-600 transition flex items-center justify-center"
     >
       <Download className="w-6 h-6" />
