f206e6337167b2efa02c7e8786afd7ae4678c647
SABDELS7 <salmiabdderrahmane@gmail.com>
2025-04-27 19:05:15 +0100

    change cv

--- src/sections/DownloadCV.jsx
+++ src/sections/DownloadCV.jsx
@@ -3,7 +3,7 @@ import { Download } from "lucide-react";
 const DownloadCVButton = () => {
   return (
     <a
-      href="/textures/CV_Abderrahmane_Salmi.pdf"
+      href="/public/textures/Abderrahmane_Salmi-CV.pdf"
       download="CV_Abderrahmane_Salmi.pdf"
       className="fixed z-[10000] bottom-20 right-4 bg-gray-800 text-white p-3 rounded-full shadow-lg hover:bg-gray-600 transition flex items-center justify-center"
     >
